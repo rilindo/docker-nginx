1b126833bbaf81b189fdcf1d874b5b0f307eefb9
Rilindo Foster <rilindo@monzell-labs.net>
2020-04-04 20:17:54 -0500

    Added playbook file

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 # I could add set -e to my script, but I find that introduce some unexpected silent failures
 # So I elected to catch errors as much as possible.
-
-PACKER_CMD=$(which packer)
 DOCKER_CMD=$(which docker)
+PACKER_CMD=$(which packer)
 PACKER_TEMPLATE="docker-nginx.json"
+ANSIBLE_PLAYBOOK_CMD=$(which ansible-playbook)
+ANSIBLE_PLAYBOOK_FILE="nginx.yml"
 
 # Note that by specifying just the os, it will pull the latest one by default
 
 DOCKER_IMAGE_IMAGES="ubuntu centos"
-
-
 
 # Check to see if commands are in the path.
 
@@ -24,6 +23,11 @@ if [ -z "${DOCKER_CMD}" ]; then
   exit 1
 fi
 
+if [ ! -e "${ANSIBLE_PLAYBOOK_CMD}" ]; then
+  echo "ansible-playbook is not installed or not in the path."
+  exit 1
+fi
+
 if [ ! -e "${PACKER_TEMPLATE}" ]; then
   echo "Packer template does not appear to exists in the current directory."
   exit 1
@@ -32,15 +36,21 @@ fi
 for IMG in ${DOCKER_IMAGE_IMAGES}
 do
   echo ${IMG}
-  packer inspect ${PACKER_TEMPLATE}
+  ${PACKER_CMD} inspect ${PACKER_TEMPLATE}
   INSPECTION_ERROR=$?
   if [ ${INSPECTION_ERROR} -ne 0 ]; then
     echo "Exiting due to inspector error with ${PACKER_TEMPLATE}"
     exit 1
   fi
-  packer validate -var "img=${IMG}" ${PACKER_TEMPLATE}
-  VALIDATION_ERROR=$?
-  if [ ${VALIDATION_ERROR} -ne 0 ]; then
+  ${PACKER_CMD} validate -var "img=${IMG}" ${PACKER_TEMPLATE}
+  PACKER_VALIDATION_ERROR=$?
+  if [ ${PACKER_VALIDATION_ERROR} -ne 0 ]; then
+    echo "Exiting due to validation error with ${PACKER_TEMPLATE}"
+    exit 1
+  fi
+  ${ANSIBLE_PLAYBOOK_CMD} --syntax-check ${ANSIBLE_PLAYBOOK_FILE}
+  ANSIBLE_PLAYBOOK_VALIDATION_ERROR=$?
+  if [ ${ANSIBLE_PLAYBOOK_VALIDATION_ERROR} -ne 0 ]; then
     echo "Exiting due to validation error with ${PACKER_TEMPLATE}"
     exit 1
   fi
